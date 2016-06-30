2d6fec966eef8463bc5303d1a241f77b55cdde5e
leonardoaraujo29 <leonardoaraujo19@yahoo.com.br>
2016-06-30 09:45:11 -0300

    Exercício 2

--- Exercicio2/src/Shape.java
+++ Exercicio2/src/Shape.java
@@ -9,5 +9,59 @@
  * @author Leonardo Araujo
  */
 public class Shape {
-    
+    private String name;
+    Shape(String aName) {name = aName;}
+    public String getName() {return name;}
+    public float calculateArea() {return 0.0f;}
+
+    public static void main(String argv[]) {
+        Circle c = new Circle("Circle C");
+        Square s = new Square("Square S");
+        Triangle t = new Triangle("Triangle T");
+        Shape shapeArray[] = {c,s,t};
+        for(int i = 0;i<shapeArray.length;i++){
+            System.out.println("The area of " + shapeArray[i].getName() + " is " + shapeArray[i].calculateArea() + " sq. cm.\n");
+        }
+    }
+}
+
+class Circle extends Shape {
+    private int radius;
+    Circle(String aName){
+        super(aName);
+        radius = 3;
+    }
+    public float calculateArea(){
+        float area;
+        area = (float) (3.14*radius*radius);
+        return area;
+    }
+}
+
+class Square extends Shape{
+    private int side;
+    Square(String aName){
+        super(aName);
+        side = 3;
+    }
+    public float calculateArea(){
+        float area;
+        area = side*side;
+        return area;
+    }
+}
+
+class Triangle extends Shape{
+    private int base;
+    private int height;
+    Triangle(String aName){
+        super(aName);
+        base = 3;
+        height = 3;
+    }
+    public float calculateArea(){
+        float area;
+        area = (float) base*height/2;
+        return area;
+    }
 }
